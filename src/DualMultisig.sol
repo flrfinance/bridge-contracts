6bff5d36872f015d0ddf3e04eb984307be4dff7a
Shresth Agrawal <shresthagrawal.31@gmail.com>
2022-09-29 19:54:34 -0400

    feat: Add draft dual multisig implementation

---
+++ src/DualMultisig.sol
@@ -0,0 +1,178 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+library Multisig {
+    error SignerAlreadyExists(address signer);
+    error SignerNotActive(address signer);
+    error MaxCommitteeSizeReached();
+    error RequestDecided(RequestStatus status);
+    error SignerSigned();
+
+    uint8 constant maxCommitteeSize = 128;
+    uint16 constant maxSignersSize = 256; // maxCommitteeSize * 2
+
+    enum RequestStatus {
+        Undecided,
+        Accepted,
+        Rejected
+    }
+
+    enum SignerStatus {
+        Uninitialized,
+        Active,
+        Removed
+    }
+
+    struct Request {
+        uint8 approvalsFirstCommittee;
+        uint8 rejectionsFirstCommittee;
+        uint8 approvalsSecondCommittee;
+        uint8 rejectionsSecondCommittee;
+        RequestStatus status;
+        uint256 approvers;
+        uint256 rejectors;
+    }
+
+    struct SignerInfo {
+        SignerStatus status;
+        uint8 index;
+        bool isFirstCommittee;
+    }
+
+    struct DualMultisig {
+        uint8 firstCommitteeAcceptanceQuorum; // slot 1 (0 - 7bits)
+        uint8 firstCommitteeRejectionQuorum; // slot 1 (8 - 15bits)
+        uint8 secondCommitteeAcceptanceQuorum; // slot 1 (16 - 23bits)
+        uint8 secondCommitteeRejectionQuorum; // slot 1 (24 - 31bits)
+        uint8 firstCommitteeSize; // slot 1 (32 - 39bits)
+        uint8 secondCommitteeSize; // slot 1 (40 - 47bits)
+        uint64 totalPoints; // slot 1 (48 - 111 bits)
+        // spare bits 112 - 144
+        uint64[maxSignersSize] points;
+        mapping(address => SignerInfo) signers;
+        mapping(uint256 => Request) requests;
+    }
+
+    function addSigner(DualMultisig storage s, address signer, bool isFirstCommittee) internal {
+        uint8 committeeSize = (isFirstCommittee ? s.firstCommitteeSize : s.secondCommitteeSize);
+        if (committeeSize == maxCommitteeSize) {
+            revert MaxCommitteeSizeReached();
+        }
+
+        SignerInfo storage signerInfo = s.signers[signer];
+        if (signerInfo.status != SignerStatus.Uninitialized) {
+            revert SignerAlreadyExists(signer);
+        }
+
+        if (isFirstCommittee) {
+            s.firstCommitteeSize++;
+        } else {
+            s.secondCommitteeSize++;
+        }
+
+        signerInfo.status = SignerStatus.Active;
+        uint8 index = s.firstCommitteeSize + s.secondCommitteeSize;
+        signerInfo.index = index;
+        signerInfo.isFirstCommittee = isFirstCommittee;
+    }
+
+    function removeSigner(DualMultisig storage s, address signer) internal {
+        SignerInfo storage signerInfo = s.signers[signer];
+        if (signerInfo.status != SignerStatus.Active) {
+            revert SignerNotActive(signer);
+        }
+        signerInfo.status = SignerStatus.Removed;
+    }
+
+    function incrementPoints(DualMultisig storage s, uint256 mask) private {
+        uint8 count = 0;
+        for (uint8 i = 0; i < maxSignersSize; i++) {
+            if ((mask & (1 << i)) != 0) {
+                s.points[i]++;
+                count++;
+            }
+        }
+        s.totalPoints += count;
+    }
+
+    function approve(DualMultisig storage s, address signer, uint256 id) internal returns (bool) {
+        SignerInfo memory signerInfo = s.signers[signer];
+        if (signerInfo.status != SignerStatus.Active) {
+            revert SignerNotActive(signer);
+        }
+        Request storage request = s.requests[id];
+        if (request.status != RequestStatus.Undecided) {
+            revert RequestDecided(request.status);
+        }
+        uint256 signerMask = 1 << signerInfo.index;
+        if ((signerMask & (request.approvers | request.rejectors)) != 0) {
+            revert SignerSigned();
+        }
+
+        request.approvers |= signerMask;
+        if (signerInfo.isFirstCommittee) {
+            ++request.approvalsFirstCommittee;
+        } else {
+            ++request.approvalsSecondCommittee;
+        }
+
+        if (
+            request.approvalsFirstCommittee >= s.firstCommitteeAcceptanceQuorum
+                && request.approvalsSecondCommittee >= s.secondCommitteeAcceptanceQuorum
+        ) {
+            request.status = RequestStatus.Accepted;
+            incrementPoints(s, request.approvers);
+            return true;
+        }
+        return false;
+    }
+
+    function reject(DualMultisig storage s, address signer, uint256 id) internal returns (bool) {
+        SignerInfo memory signerInfo = s.signers[signer];
+        if (signerInfo.status != SignerStatus.Active) {
+            revert SignerNotActive(signer);
+        }
+        Request storage request = s.requests[id];
+        if (request.status != RequestStatus.Undecided) {
+            revert RequestDecided(request.status);
+        }
+        uint256 signerMask = 1 << signerInfo.index;
+        if ((signerMask & (request.approvers | request.rejectors)) != 0) {
+            revert SignerSigned();
+        }
+
+        request.rejectors |= signerMask;
+        bool rejectionQuorumReached;
+        if (signerInfo.isFirstCommittee) {
+            rejectionQuorumReached = (++request.rejectionsFirstCommittee) > s.firstCommitteeRejectionQuorum;
+        } else {
+            rejectionQuorumReached = (++request.rejectionsSecondCommittee) > s.secondCommitteeRejectionQuorum;
+        }
+
+        if (rejectionQuorumReached) {
+            request.status = RequestStatus.Rejected;
+            incrementPoints(s, request.rejectors);
+            return true;
+        }
+        return false;
+    }
+
+    function clearPoints(DualMultisig storage s, address signer) internal {
+        uint8 index = s.signers[signer].index;
+        uint64 p = s.points[index];
+        s.points[index] = 0;
+        s.totalPoints -= p;
+    }
+
+    function status(DualMultisig storage s, uint256 id) internal view returns (RequestStatus) {
+        return s.requests[id].status;
+    }
+
+    function totalPoints(DualMultisig storage s) internal view returns (uint64) {
+        return s.totalPoints;
+    }
+
+    function points(DualMultisig storage s, address signer) internal view returns (uint64) {
+        return s.points[s.signers[signer].index];
+    }
+}
---
+++ src/pseudo-code.md
@@ -0,0 +1,148 @@
+class DualMultisig
+    n, m, p, q
+    requests (id => {
+        approvalsA, 
+        rejectionsA, 
+        approvalsB, 
+        rejectionsB, 
+        status (0, 1, -1),
+        rejectors: address[],
+        approvers: address[],
+        signer: (address => boolean),
+    })
+    points
+    totalPoints 
+    approve(address, id, isA):
+        request = requests[id]
+        require that the request status is undecided
+        require that the request.signer[address] is false
+        request.signer[address] = true
+        request.approvers.add(address);
+        (isA ? request.approvalsA : request.approvalsB)++;
+        if request.approvalsA >= n and request.approvalsB >= p:
+            request.status = approved
+            totalPoints += approvers.length;
+            for validator in request.approvers:
+                points[validator]++
+            return true
+        return false
+    reject(address, id, isA)
+        request = requests[id]
+        require that the request status is undecided
+        require that the request.signer[address] is false
+        request.signer[address] = true
+        request.rejectors.add(address);
+        (isA ? request.rejectionsA : request.rejectionsB)++
+        if (isA ? request.rejectionsA == n : request.rejectionsB == p):
+            request.status = rejected
+            totalPoints += rejectors.length;
+            for validators in request.rejectors:
+                points[validators]++
+            return true
+        return false
+    status(id):
+        requests[id].status
+    totalPoints():
+        totalPoints
+    points(address)
+        points[address]
+    clearPoints(address)
+        totalPoints -= points[address]
+        points[address] = 0
+
+abstract BridgeCommon is AccessControl
+    ---
+    abstract onDeposit(id, {token, amount, to})
+    abstract onApprove(id, {token, amount, to})
+    ---
+    depositCount;
+    deposit({token, amount, to}) => id
+        bridge is not paused
+        token is whitelisted
+        amount is range of min, max token amount
+        id = ++depositCount
+        amountDeposited = onDeposit(id, {token, amount, to})
+        emit Deposit(id, token, amountDeposited, to)
+    ---
+    requests;
+    DualMultisig multisig;
+    _approve(id, type):
+        address is whitelisted with type
+        isApproved = multisig.approve(address, id, type)
+        if isApproved:
+            onApprove(id, {token, amount, to})
+    approve(id, type, {token, amount, to}):
+        bridge is not paused
+        if request of id exist
+            token, amount and to matches the 
+            existing request
+        else 
+            token is whitelisted
+            amount is range of min, max token amount
+            add to requests
+        _approve(id, type)
+    approve(id, type):
+        bridge is not paused
+        request exist with id
+        _approve(id, type)
+    reject(id, type):
+        bridge is not paused 
+        address is whitelisted with type
+        multisig.reject(address, id, type)
+    ---
+    ... access control methods ...
+    whitelist({token, min, max}) only owner 
+    pause() only owner
+.
+BridgeEVM is BridgeCommon
+    accumalatedValidatorFees(token):
+        return balanceOf token
+    validatorFees({token, amount, to}):
+        return fix percent of amount
+    claimValidatorFees(type):
+        address is whitelisted with type
+        p = multisig.getPoints(address)
+        t = multisig.totalPoints()
+        multisigOfType.clearPoints(address)
+        for c of whitelisted tokens
+            transfer (accumalatedValidatorFees(c) * p / t) tokens of c
+    ---
+    private onDeposit(id, {token, amount, to}):
+        transfer user tokens from user to custodian
+    private onApprove(id, {token, amount, to}):
+        vFee = validatorFees({token, amount, to})
+        emit redeem(id, token, amount - vFee, to, vFee);
+.
+BridgeFLR is BridgeCommon
+    accumalatedProtocolFees
+    accumalatedValidatorFees(token):
+        return balanceOf token - accumalatedProtocolFees[token] 
+    protocolFees({token, amount, to}):
+        return fix percent of amount
+    validatorFees({token, amount, to}):
+        return fix percent of amount
+    claimValidatorFees(type):
+        address is whitelisted with type
+        p = multisig.getPoints(address)
+        t = multisig.totalPoints()
+        multisigOfType.clearPoints(address)
+        for c of whitelisted tokens
+            transfer (accumalatedValidatorFees(c) * p / t) tokens of c
+    claimProtocolFees(token):
+        address is owner
+        pfee = accumalatedProtocolFees[token]
+        accumalatedProtocolFees[token] -= pfee
+        transfer pfee amount of tokens to address
+    ---
+    private onDeposit(id, {token, amount, to}):
+        pFee = protocolFees({token, amount, to})
+        accumalatedProtocolFees[token] += pFee
+        transfer pFee user tokens to this contracts
+        burn amount - pFee user tokens
+        return amount - pFee
+    private onApprove(id, {token, amount, to}):
+        mint amount tokens
+        pFee = protocolFees({token, amount, to})
+        vFee = protocolFees({token, amount, to})
+        accumalatedProtocolFees[token] += pFee
+        transfer amount - pFee - vFee tokens to the user
