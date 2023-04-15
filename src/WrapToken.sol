5da63799cc72b4533aec31b757ef157788464291
Apostolos Tzinas <26859484+tzinas@users.noreply.github.com>
2023-04-15 04:20:37 +0300

    Add license (#18)
    * Add FLR license to contracts
    * Update LICENSE file
    * Fix quotation marks

--- src/WrapToken.sol
+++ src/WrapToken.sol
@@ -1,6 +1,24 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.15;
 
+/**
+ * Copyright (C) 2023 Flare Finance B.V. - All Rights Reserved.
+ *
+ * This source code and any functionality deriving from it are owned by Flare
+ * Finance BV and the use of it is only permitted within the official platforms
+ * and/or original products of Flare Finance B.V. and its licensed parties. Any
+ * further enquiries regarding this copyright and possible licenses can be directed
+ * to partners@flr.finance.
+ *
+ * The source code and any functionality deriving from it are provided "as is",
+ * without warranty of any kind, express or implied, including but not limited to
+ * the warranties of merchantability, fitness for a particular purpose and
+ * noninfringement. In no event shall the authors or copyright holder be liable
+ * for any claim, damages or other liability, whether in an action of contract,
+ * tort or otherwise, arising in any way out of the use or other dealings or in
+ * connection with the source code and any functionality deriving from it.
+ */
+
 import {
     ERC20PresetMinterPauser
 } from "@openzeppelin/contracts/token/ERC20/presets/ERC20PresetMinterPauser.sol";
